f66b64b57f0621e68632e5455b5a02043f0b25c7
jlucea <jonathan.lucea@gmail.com>
2024-07-05 17:00:23 +0200

    Database done

---
+++ src/Form/BookType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Entity\Editor;
+use App\Enum\BookStatus;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('isbn', TextType::class)
+            ->add('cover', TextType::class)
+            ->add('editedAt', DateType::class, [
+                'input' => 'datetime_immutable',
+                'widget' => 'single_text',
+            ])
+            ->add('plot', TextType::class)
+            ->add('pageNumber', IntegerType::class)
+            ->add('status', EnumType::class, [
+                'class' => BookStatus::class
+            ])
+            ->add('editor', EntityType::class, [
+                'class' => Editor::class,
+                'choice_label' => 'name',
+            ])
+            ->add('authors', EntityType::class, [
+                'class' => Author::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'by_reference' => false,
+            ])
+            ->add('certification', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'Je certifie l\'exactitude des informations ci-dessus',
+                'constraints' => [
+                    new Assert\IsTrue(message: 'Vous devez cocher cette case pour ajouter un livre'),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
